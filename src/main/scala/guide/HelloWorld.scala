37e352d9b7c4eed01c73327efb7081e3c739f46c
Jason Zaugg <jzaugg@gmail.com>
2015-08-11 14:09:54 +1000

    More on trees.

--- src/main/scala/guide/HelloWorld.scala
+++ src/main/scala/guide/HelloWorld.scala
@@ -1,30 +1,31 @@
 package guide
-
 /** Basic introduction to trees via runtime reflection and the toolbox compiler */
 object HelloWorld {
 
-   def main(args: Array[String]){
-     val cm = scala.reflect.runtime.currentMirror
-     import cm.universe._
+  def main(args: Array[String]){
+    val cm = scala.reflect.runtime.currentMirror
+    import cm.universe._
 
-     ===("It starts with a tree.")
-     def tree = Apply(Ident(TermName("println")), Literal(Constant("hello world")) :: Nil)
+    ===("It starts with a tree.")
+    // println("hello world")
+    def tree = Apply(Ident(TermName("println")), Literal(Constant("hello world")) :: Nil)
 
-     p("// We can print the raw representation of a tree")
-     p(showRaw(tree))
-     p("// or, a prettier view as code")
-     p(show(tree))
-     p("// or, source code that could be reparsed")
-     p(showCode(tree))
+    p("// showRaw: print the raw representation of a tree")
+    p(showRaw(tree))
+    p("// show: view as something like source code")
+    p(show(tree))
+    p("// showCode: source code that could be reparsed")
+    p(showCode(tree))
 
-     ===("Tree, meet types")
-     import scala.tools.reflect.ToolBox
-     val toolbox = cm.mkToolBox()
-     val typedTree = toolbox.typecheck(tree)
-     p(show(typedTree, printTypes = true))
+    ===("Tree, meet types")
+    import scala.tools.reflect.ToolBox
+    val toolbox = cm.mkToolBox()
+    val typedTree = toolbox.typecheck(tree)
+    p(show(typedTree, printTypes = true))
 
-     ===("Typecheck + code generation + evaluation")
+    ===("Typecheck + code generation + evaluation")
 
-     toolbox.eval(tree)
-   }
- }
+    toolbox.eval(tree)
+  }
+}
+
